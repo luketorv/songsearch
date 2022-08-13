b5fac862ee77dcb835d2cdc9669a02629dc2c0cb
luketorv <104324965+luketorv@users.noreply.github.com>
2022-08-13 14:29:13 -0500

    handlebars layout, connected server, added bulma

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+const path = require('path')
+const express = require('express');
 const exphbs = require('express-handlebars');
+const app = express();
+const port = process.env.port || 3001;
+
+
+
+
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname,'public')))
+app.get("/",(req,res)=>{
+    res.render("homepage")
+})
+app.listen(port, ()=>console.log("listening"))
