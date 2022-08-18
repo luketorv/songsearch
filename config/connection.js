144b3a8217cd71c9dad0a05f0aa224e7e8cfa1d1
luketorv <104324965+luketorv@users.noreply.github.com>
2022-08-18 12:27:37 -0500

    connect jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,15 @@
-//const Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 require('dotenv').config();
-
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         port: 3306,
+//     }
+// );
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -17,6 +26,5 @@ if (process.env.JAWSDB_URL) {
     },
   );
 }
-
 
 module.exports = sequelize;
