21f895ca137ec145976dac1829aea7692647d8c7
luketorv <104324965+luketorv@users.noreply.github.com>
2022-08-20 12:28:21 -0500

    models

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 // import all models
-const Post = require('./Post');
-const User = require('./User');
-const Comment = require('./Comment');
+const Post = require('./post');
+const User = require('./user');
+const Comment = require('./comment');
 
 // create associations
 User.hasMany(Post, {
